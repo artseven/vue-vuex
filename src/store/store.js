67f3f9961f9e877c905c926e1e9ffde8e5466a7b
artseven <yabkevich.artsiom.09@gmail.com>
2019-12-09 12:29:32 -0500

    outsorcing all the methods into separate files

--- src/store/store.js
+++ src/store/store.js
@@ -1,40 +1,21 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import counter from './modules/counter';
+import counter from "./modules/counter";
+
+import * as actions from "./actions";
+import * as mutations from "./mutations";
+import * as getters from "./getters";
 
 Vue.use(Vuex);
 
 export const store = new Vuex.Store({
   state: {
-		value: 0
+    value: 0
   },
-  getters: {
-
-		value: state => {
-			return state.value;
-		}
-  },
-  mutations: {
-
-		updateValue: (state, payload) => {
-			state.value = payload;
-		}
-  },
-  actions: {
-    // increment: context => {
-    // 	context.commit('increment');
-
-    // }
-    //es5 version
-    //vs
-    //es6 version
-
-		updateValue: ({commit}, payload) => {
-			commit('updateValue', payload);
-		}
-
-	},
-	modules: {
-		counter
-	}
+  getters,
+  mutations,
+  actions,
+  modules: {
+    counter
+  }
 });
